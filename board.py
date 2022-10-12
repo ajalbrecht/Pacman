757d340e0f2c3583f7c98d1836e87c60bba0f360
ajalbrecht <ajalbrechttwin@gmail.com>
2022-10-12 16:58:05 -0700

    testing basic board functions

---
+++ board.py
@@ -0,0 +1,64 @@
+import pygame as pg
+from pygame.sprite import Sprite, Group
+
+class Board(Sprite): 
+    def __int__ ():
+        # 0=wall   1=open_space_with_food    2=open_space_without_food   3=gghost_house_door
+        self.game_board[[0,0,0,0,0,0,0],
+                        [0,1,1,2,1,1,0],
+                        [0,1,0,3,0,1,0],
+                        [0,1,2,2,2,1,0],
+                        [0,1,0,0,0,1,0],
+                        [0,1,1,2,1,1,0],
+                        [0,0,0,0,0,0,0]]
+
+        self.Pacman = [[0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,1,0,0,0],
+                       [0,0,0,0,0,0,0]]
+
+        self.Inky =   [[0,0,0,0,0,0,0],
+                       [0,0,0,1,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0]]
+        
+        self.Blinky = [[0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,1,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0]]
+        
+        self.Pinky =  [[0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0]]
+        
+        self.Clide =  [[0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0],
+                       [0,0,0,0,0,0,0]]
+    
+    def GetPacmanLocation():
+        for x in range(len(self.game_board)):
+            for y in range(len(self.game_board[x])):
+                if [x][y] == 1:
+                    print("why hello there")
+
+        
+
+        #want to.. set pos for each, return pos for each, reset all board, 
+        #add later (poettialy draw board based upon array, gosts should still have their own draw)
