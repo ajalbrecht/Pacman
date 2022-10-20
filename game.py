57fae2b4dcbb86894e44936ef9d525a4093e4b4b
MarkCarbajal <markc9090@csu.fullerton.edu>
2022-10-20 13:08:36 -0700

    Added start button, gf, gamestats

--- game.py
+++ game.py
@@ -3,16 +3,24 @@ import pygame.locals
 import settings 
 from pac import Pacman
 from board import Graph
+from gamestats import GameStats
+from button import Button
+import game_functions as gf
 
 class GameController(object):
     def __init__(self):
         pygame.init()
+        self.stats = GameStats(settings)
+        self.settings = settings
         self.screen = pygame.display.set_mode(settings.SCREENSIZE, 0, 32)
         self.background = None
         self.clock = pygame.time.Clock()
         # testing game board commands
         self.graph = Graph(self.screen)
         print(self.graph.SetCharacterLocation(3,2,1))
+
+        self.play_button = Button(self.settings, self.screen, "Play")
+        self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
 
     def setBackground(self):
         self.background = pygame.surface.Surface(settings.SCREENSIZE).convert()
@@ -23,14 +31,18 @@ class GameController(object):
         self.pacman = Pacman()
 
     def update(self):
-        dt = self.clock.tick(30) / 1000.0
-        self.checkEvents()
-        self.render()
-        self.pacman.update(dt)
-    def checkEvents(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
+        while self.stats.game_active == False:
+            self.screen.fill((0,0,0))
+            self.screen.blit(self.background,(0,0))
+            self.play_button.draw_button()
+            gf.check_events(settings=self.settings, stats=self.stats, play_button=self.play_button)
+            pygame.display.flip()
+
+        while self.stats.game_active == True:
+                dt = self.clock.tick(30) / 1000.0
+                self.render()
+                self.pacman.update(dt)
+                gf.check_events(settings=self.settings, stats=self.stats, play_button=self.play_button)
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
---
+++ game_functions.py
@@ -0,0 +1,13 @@
+import sys
+import pygame as pg
+
+def check_events(settings, stats, play_button):
+    for event in pg.event.get():
+        if event.type == pg.QUIT: sys.exit()
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+            
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
