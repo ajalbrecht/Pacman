b6bfa0e694296845e81a476590b29db8067558a4
ajalbrecht <54691648+ajalbrecht@users.noreply.github.com>
2022-10-22 02:35:11 -0700

    Update ghost.py
    force correct changes to gost

--- ghost.py
+++ ghost.py
@@ -9,15 +9,22 @@ from sys import exit
 class Ghost(Sprite):
     Ghost_images = [pg.transform.rotozoom(pg.image.load(f'images/red-ghost.png'), 0, 1.0)]
     
-    def __init__(self):
-        self.name = settings.GHOST
+    def __init__(self,game):
+        self.game = game
+        self.screen = game
+        #self.settings = game.settings
         self.speed = 100
         self.image = pg.image.load('images/red-ghost.png')
 
-    def update(self):	
-        self.render()
-        
-    def render(self):
-    
-        pg.image.load('images/red-ghost.png')
+        self.rect = self.image.get_rect()
+        #self.screen_rect = game.screen.get_rect()
+       
+        self.vel = Vector(200, 400)
+   
+    def render(self, screen):
+        #self.update()
+        self.rect = (200,400)
+        #pg.image.load('images/red-ghost.png')
+        self.screen.blit(self.image, (200, 400))
 
+        
