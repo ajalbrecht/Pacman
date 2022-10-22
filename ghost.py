14e79bae0e051d8457b3a8f6bf9a5a75420abaf5
brauliom7 <98502368+brauliom7@users.noreply.github.com>
2022-10-22 01:09:02 -0700

    Update ghost.py

--- ghost.py
+++ ghost.py
@@ -9,23 +9,15 @@ from sys import exit
 class Ghost(Sprite):
     Ghost_images = [pg.transform.rotozoom(pg.image.load(f'images/red-ghost.png'), 0, 1.0)]
     
-    def __init__(self,game):
-        self.game = game
-        self.screen = game.screen
-        self.settings = game.settings
+    def __init__(self):
+        self.name = settings.GHOST
         self.speed = 100
         self.image = pg.image.load('images/red-ghost.png')
 
-        self.rect = self.image.get_rect()
-        self.screen_rect = game.screen.get_rect()
-       
-        self.vel = Vector()
-
     def update(self):	
-        self.posn += self.vel
         self.render()
-   
-    def render(self, screen):
+        
+    def render(self):
     
         pg.image.load('images/red-ghost.png')
 
